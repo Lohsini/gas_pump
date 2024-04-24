3ac79f07bf4965a34e88d25006ca91325cac3803
Lohisni <lohi3152@gmail.com>
2024-04-24 13:40:04 -0700

    state connect ok

--- main.cpp
+++ main.cpp
@@ -3,60 +3,94 @@
 #include <cctype>
 #include "main.h"
 
-void MDAEFSM::Activate() {
-    s->Activate();
+MDAEFSM::MDAEFSM() : s0(this), s1(this), s2(this), s3(this), s4(this), s5(this), s6(this), start(this)
+{
+  LS[0] = &s0;
+  LS[1] = &s1;
+  LS[2] = &s2;
+  LS[3] = &s3;
+  LS[4] = &s4;
+  LS[5] = &s5;
+  LS[6] = &s6;
+  LS[7] = &start;
+  s = LS[7];
 }
 
-void MDAEFSM::Start() {
-    s->Start();
+void MDAEFSM::Activate()
+{
+  std::cout << "0000000000" << std::endl;
+  s->Activate();
 }
 
-void MDAEFSM::PayType(int t) {
-    s->PayType(t);
+void MDAEFSM::Start()
+{
+  s->Start();
 }
 
-void MDAEFSM::Reject() {
-    s->Reject();
+void MDAEFSM::PayType(int t)
+{
+  s->PayType(t);
 }
 
-void MDAEFSM::Cancel() {
-    s->Cancel();
+void MDAEFSM::Reject()
+{
+  s->Reject();
 }
 
-void MDAEFSM::Approved() {
-    s->Approved();
+void MDAEFSM::Cancel()
+{
+  s->Cancel();
 }
 
-void MDAEFSM::StartPump() {
-    s->StartPump();
+void MDAEFSM::Approved()
+{
+  s->Approved();
 }
 
-void MDAEFSM::Pump() {
-    s->Pump();
+void MDAEFSM::StartPump()
+{
+  s->StartPump();
 }
 
-void MDAEFSM::StopPump() {
-    s->StopPump();
+void MDAEFSM::Pump()
+{
+  s->Pump();
 }
 
-void MDAEFSM::SelectGas(int g) {
-    s->SelectGas(g);
+void MDAEFSM::StopPump()
+{
+  s->StopPump();
 }
 
-void MDAEFSM::Receipt() {
-    s->Receipt();
+void MDAEFSM::SelectGas(int g)
+{
+  s->SelectGas(g);
 }
 
-void MDAEFSM::NoReceipt() {
-    s->NoReceipt();
+void MDAEFSM::Receipt()
+{
+  s->Receipt();
 }
 
-void MDAEFSM::Continue() {
-    s->Continue();
+void MDAEFSM::NoReceipt()
+{
+  s->NoReceipt();
 }
 
-void MDAEFSM::ChangeState(int x) {
-    s = LS[x];
+void MDAEFSM::Continue()
+{
+  s->Continue();
+}
+
+void MDAEFSM::ChangeState(int x)
+{
+  std::cout << "Change State to S" << x << std::endl;
+  s = LS[x];
+}
+
+State::State(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
 void State::Activate() {}
@@ -85,131 +119,249 @@ void State::NoReceipt() {}
 
 void State::Continue() {}
 
-void Start::Activate() {}
-
-void S0::Start() {}
-
-void S1::PayType(int t) {}
-
-void S2::Reject() {}
-
-void S2::Approved() {}
-
-void S3::Cancel() {}
-
-void S3::Continue() {}
-
-void S3::SelectGas(int g) {}
-
-void S4::StartPump() {}
-
-void S5::Pump() {}
-
-void S5::StopPump() {}
-
-void S6::NoReceipt() {}
-
-void S6::Receipt() {}
-
-GP1::GP1(DataStore1 *ds) {
-    this->d = ds;
+Start::Start(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP1::Activate(int a) {
-    std::cout << "Activate GasPump price with parameter: " << a << std::endl;
+void Start::Activate()
+{
+  p->ChangeState(0);
 }
 
-void GP1::Start() {
-    std::cout << "Start GasPump" << std::endl;
+S0::S0(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP1::PayCredit() {
-    std::cout << "Pay with credit" << std::endl;
+void S0::Start()
+{
+  p->ChangeState(0);
 }
 
-void GP1::Reject() {
-    std::cout << "Transaction rejected" << std::endl;
+S1::S1(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP1::Cancel() {
-    std::cout << "Transaction cancelled" << std::endl;
+void S1::PayType(int t)
+{
+  if (t == 1)
+  {
+    p->ChangeState(2);
+  }
+  else
+  {
+    p->ChangeState(3);
+  }
 }
 
-void GP1::Approved() {
-    std::cout << "Transaction approved" << std::endl;
+S2::S2(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP1::PayCash(int c) {
-    std::cout << "Pay with cash: " << c << std::endl;
+void S2::Reject()
+{
+  p->ChangeState(0);
 }
 
-void GP1::StartPump() {
-    std::cout << "Start pumping gas" << std::endl;
+void S2::Approved()
+{
+  p->ChangeState(3);
 }
 
-void GP1::Pump() {
-    std::cout << "Pumping gas" << std::endl;
+S3::S3(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP1::StopPump() {
-    std::cout << "Stop pumping gas" << std::endl;
+void S3::Cancel()
+{
+  p->ChangeState(0);
 }
 
-GP2::GP2(DataStore2 *ds) {
-    this->d = ds;
+void S3::Continue()
+{
+  p->ChangeState(4);
 }
 
-void GP2::Activate(float a, float b, float c) {
-    std::cout << "Activate GasPump with Regular/Premium/Diesel: " << a << b << c << std::endl;
+void S3::SelectGas(int g)
+{
+  // no change state
 }
 
-void GP2::Start() {
-    std::cout << "Start GasPump" << std::endl;
+S4::S4(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP2::PayCash(int c) {
-    std::cout << "Pay with cash: " << c << std::endl;
+void S4::StartPump()
+{
+  p->ChangeState(5);
 }
 
-void GP2::Cancel() {
-    std::cout << "Transaction cancelled" << std::endl;
+S5::S5(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP2::Premium() {
-    std::cout << "Premium gas selected" << std::endl;
+void S5::Pump()
+{
+  // no change state
 }
 
-void GP2::Regular() {
-    std::cout << "Regular gas selected" << std::endl;
+void S5::StopPump()
+{
+  p->ChangeState(6);
 }
 
-void GP2::Diesel() {
-    std::cout << "Diesel gas selected" << std::endl;
+S6::S6(MDAEFSM *mda)
+{
+  this->p = mda;
 }
 
-void GP2::StartPump() {
-    std::cout << "Start pumping gas" << std::endl;
+void S6::NoReceipt()
+{
+  p->ChangeState(0);
 }
 
-void GP2::PumpGallon() {
-    std::cout << "One gallon of gas is disposed" << std::endl;
+void S6::Receipt()
+{
+  p->ChangeState(0);
 }
 
-void GP2::Stop() {
-    std::cout << "Stop pumping gas" << std::endl;
+GP1::GP1(DataStore1 *ds, MDAEFSM *mda)
+{
+  this->d = ds;
+  this->m = mda;
 }
 
-void GP2::Receipt() {
-    std::cout << "Receipt is requested" << std::endl;
+void GP1::Activate(int a)
+{
+  std::cout << "Activate GasPump price with parameter: " << a << std::endl;
+  if (a > 0)
+  {
+    ((DataStore1 *)d)->temp_a = a;
+    m->Activate();
+  }
 }
 
-void GP2::NoReceipt() {
-    std::cout << "No receipt" << std::endl;
+void GP1::Start()
+{
+  std::cout << "Start GasPump" << std::endl;
 }
 
-int main() {
+void GP1::PayCredit()
+{
+  std::cout << "Pay with credit" << std::endl;
+}
+
+void GP1::Reject()
+{
+  std::cout << "Transaction rejected" << std::endl;
+}
+
+void GP1::Cancel()
+{
+  std::cout << "Transaction cancelled" << std::endl;
+}
+
+void GP1::Approved()
+{
+  std::cout << "Transaction approved" << std::endl;
+}
+
+void GP1::PayCash(int c)
+{
+  std::cout << "Pay with cash: " << c << std::endl;
+}
+
+void GP1::StartPump()
+{
+  std::cout << "Start pumping gas" << std::endl;
+}
+
+void GP1::Pump()
+{
+  std::cout << "Pumping gas" << std::endl;
+}
+
+void GP1::StopPump()
+{
+  std::cout << "Stop pumping gas" << std::endl;
+}
+
+GP2::GP2(DataStore2 *ds)
+{
+  this->d = ds;
+}
+
+void GP2::Activate(float a, float b, float c)
+{
+  std::cout << "Activate GasPump with Regular/Premium/Diesel: " << a << b << c << std::endl;
+}
+
+void GP2::Start()
+{
+  std::cout << "Start GasPump" << std::endl;
+}
+
+void GP2::PayCash(int c)
+{
+  std::cout << "Pay with cash: " << c << std::endl;
+}
+
+void GP2::Cancel()
+{
+  std::cout << "Transaction cancelled" << std::endl;
+}
+
+void GP2::Premium()
+{
+  std::cout << "Premium gas selected" << std::endl;
+}
+
+void GP2::Regular()
+{
+  std::cout << "Regular gas selected" << std::endl;
+}
+
+void GP2::Diesel()
+{
+  std::cout << "Diesel gas selected" << std::endl;
+}
+
+void GP2::StartPump()
+{
+  std::cout << "Start pumping gas" << std::endl;
+}
+
+void GP2::PumpGallon()
+{
+  std::cout << "One gallon of gas is disposed" << std::endl;
+}
+
+void GP2::Stop()
+{
+  std::cout << "Stop pumping gas" << std::endl;
+}
+
+void GP2::Receipt()
+{
+  std::cout << "Receipt is requested" << std::endl;
+}
+
+void GP2::NoReceipt()
+{
+  std::cout << "No receipt" << std::endl;
+}
+
+int main()
+{
   DataStore1 ds1;
   DataStore2 ds2;
+  MDAEFSM mda;
   std::string choice;
   int a, b, c;
 
@@ -220,9 +372,11 @@ int main() {
   std::cin >> choice;
   std::cout << std::endl;
 
-  if (choice == "1") {
-    GP1 gp1(&ds1);
-    while (choice != "q") {
+  if (choice == "1")
+  {
+    GP1 gp1(&ds1, &mda);
+    while (choice != "q")
+    {
       std::cout << " ----- GasPump-1 MENU of Operations ----- " << std::endl;
       std::cout << "0. Activate(int)" << std::endl;
       std::cout << "1. Start()" << std::endl;
@@ -237,65 +391,73 @@ int main() {
       std::cout << "q. Quit the program" << std::endl;
       std::cout << "Select Operation: ";
       std::cin >> choice;
-      if (choice == "q"){
+      if (choice == "q")
+      {
         break;
       }
-      
-      if (std::stoi(choice) >= 0 && std::stoi(choice) <= 9) {
+
+      if (std::stoi(choice) >= 0 && std::stoi(choice) <= 9)
+      {
         std::cout << " ***** Operation:";
-        switch (std::stoi(choice)) {
-          case 0:
-            std::cout << "Activate(int a)" << std::endl;
-            std::cout << "Enter value of the parameter a: ";
-            std::cin >> a;
-            gp1.Activate(a);
-            break;
-          case 1:
-            std::cout << "Start()" << std::endl;
-            gp1.Start();
-            break;
-          case 2:
-            std::cout << "PayCredit()" << std::endl;
-            gp1.PayCredit();
-            break;
-          case 3:
-            std::cout << "Reject()" << std::endl;
-            gp1.Reject();
-            break;
-          case 4:
-            std::cout << "Cancel()" << std::endl;
-            gp1.Cancel();
-            break;
-          case 5:
-            std::cout << "Approved()" << std::endl;
-            gp1.Approved();
-            break;
-          case 6:
-            std::cout << "PayCash(int c)" << std::endl;
-            std::cout << "Enter value of the parameter c: ";
-            std::cin >> c;
-            gp1.PayCash(c);
-            break;
-          case 7:
-            std::cout << "StartPump()" << std::endl;
-            gp1.StartPump();
-            break;
-          case 8:
-            std::cout << "Pump()" << std::endl;
-            gp1.Pump();
-            break;
-          case 9:
-            std::cout << "StopPump()" << std::endl;
-            gp1.StopPump();
-            break;
+        switch (std::stoi(choice))
+        {
+        case 0:
+          std::cout << "Activate(int a)" << std::endl;
+          std::cout << "Enter value of the parameter a: ";
+          std::cin >> a;
+          gp1.Activate(a);
+          break;
+        case 1:
+          std::cout << "Start()" << std::endl;
+          gp1.Start();
+          break;
+        case 2:
+          std::cout << "PayCredit()" << std::endl;
+          gp1.PayCredit();
+          break;
+        case 3:
+          std::cout << "Reject()" << std::endl;
+          gp1.Reject();
+          break;
+        case 4:
+          std::cout << "Cancel()" << std::endl;
+          gp1.Cancel();
+          break;
+        case 5:
+          std::cout << "Approved()" << std::endl;
+          gp1.Approved();
+          break;
+        case 6:
+          std::cout << "PayCash(int c)" << std::endl;
+          std::cout << "Enter value of the parameter c: ";
+          std::cin >> c;
+          gp1.PayCash(c);
+          break;
+        case 7:
+          std::cout << "StartPump()" << std::endl;
+          gp1.StartPump();
+          break;
+        case 8:
+          std::cout << "Pump()" << std::endl;
+          gp1.Pump();
+          break;
+        case 9:
+          std::cout << "StopPump()" << std::endl;
+          gp1.StopPump();
+          break;
         }
-      } else if (choice != "q") {
-          std::cout << "Invalid choice" << std::endl;
+      }
+      else if (choice != "q")
+      {
+        std::cout << "Invalid choice" << std::endl;
       }
     }
-  } else if (choice == "2") {
+  }
+  else if (choice == "2")
+  {
     GP2 gp2(&ds2);
-    while (choice != "q") {
+    while (choice != "q")
+    {
       std::cout << "----- GasPump-2 MENU of Operations -----" << std::endl;
       std::cout << "0. Activate(float, float, float)" << std::endl;
       std::cout << "1. Start()" << std::endl;
@@ -312,71 +474,78 @@ int main() {
       std::cout << "q. Quit the program" << std::endl;
       std::cout << "Select Operation: ";
       std::cin >> choice;
-      if (choice == "q"){
+      if (choice == "q")
+      {
         break;
       }
-      
-      if (std::stoi(choice) >= 0 && std::stoi(choice) <= 11) {
+
+      if (std::stoi(choice) >= 0 && std::stoi(choice) <= 11)
+      {
         std::cout << "Operation: ";
-        switch (std::stoi(choice)) {
-          case 0:
-            std::cout << "Activate(float, float, float)" << std::endl;
-            std::cout << "Enter value of parameters with Regular/Premium/Diesel: ";
-            std::cin >> a >> b >> c;
-            gp2.Activate(a, b, c);
-            break;
-          case 1:
-            std::cout << "Start()" << std::endl;
-            gp2.Start();
-            break;
-          case 2:
-            std::cout << "PayCash(int)" << std::endl;
-            std::cout << "Enter value of the parameter cash: ";
-            std::cin >> c;
-            gp2.PayCash(c);
-            break;
-          case 3:
-            std::cout << "Cancel()" << std::endl;
-            gp2.Cancel();
-            break;
-          case 4:
-            std::cout << "Premium()" << std::endl;
-            gp2.Premium();
-            break;
-          case 5:
-            std::cout << "Regular()" << std::endl;
-            gp2.Regular();
-            break;
-          case 6:
-            std::cout << "Diesel" << std::endl;
-            gp2.Diesel();
-            break;
-          case 7:
-            std::cout << "StartPump()" << std::endl;
-            gp2.StartPump();
-            break;
-          case 8:
-            std::cout << "PumpGallon()" << std::endl;
-            gp2.PumpGallon();
-            break;
-          case 9:
-            std::cout << "Stop()" << std::endl;
-            gp2.Stop();
-            break;
-          case 10:
-            std::cout << "Receipt()" << std::endl;
-            gp2.Receipt();
-            break;
-          case 11:
-            std::cout << "NoReceipt()" << std::endl;
-            gp2.NoReceipt();
-            break;
+        switch (std::stoi(choice))
+        {
+        case 0:
+          std::cout << "Activate(float, float, float)" << std::endl;
+          std::cout << "Enter value of parameters with Regular/Premium/Diesel: ";
+          std::cin >> a >> b >> c;
+          gp2.Activate(a, b, c);
+          break;
+        case 1:
+          std::cout << "Start()" << std::endl;
+          gp2.Start();
+          break;
+        case 2:
+          std::cout << "PayCash(int)" << std::endl;
+          std::cout << "Enter value of the parameter cash: ";
+          std::cin >> c;
+          gp2.PayCash(c);
+          break;
+        case 3:
+          std::cout << "Cancel()" << std::endl;
+          gp2.Cancel();
+          break;
+        case 4:
+          std::cout << "Premium()" << std::endl;
+          gp2.Premium();
+          break;
+        case 5:
+          std::cout << "Regular()" << std::endl;
+          gp2.Regular();
+          break;
+        case 6:
+          std::cout << "Diesel" << std::endl;
+          gp2.Diesel();
+          break;
+        case 7:
+          std::cout << "StartPump()" << std::endl;
+          gp2.StartPump();
+          break;
+        case 8:
+          std::cout << "PumpGallon()" << std::endl;
+          gp2.PumpGallon();
+          break;
+        case 9:
+          std::cout << "Stop()" << std::endl;
+          gp2.Stop();
+          break;
+        case 10:
+          std::cout << "Receipt()" << std::endl;
+          gp2.Receipt();
+          break;
+        case 11:
+          std::cout << "NoReceipt()" << std::endl;
+          gp2.NoReceipt();
+          break;
         }
-      } else if (choice != "q") {
-          std::cout << "Invalid choice" << std::endl;
+      }
+      else if (choice != "q")
+      {
+        std::cout << "Invalid choice" << std::endl;
       }
     }
-  } else {
+  }
+  else
+  {
     std::cout << "Invalid choice. Exiting..." << std::endl;
   }
   return 0;
--- main.h
+++ main.h
@@ -3,155 +3,191 @@
 
 #include <iostream>
 
-class State; // Forward declaration
+class MDAEFSM;
 
-class MDAEFSM {
-    State *s;
-    State *LS[8];
-  public:
-    void Activate();
-    void Start();
-    void PayType(int t);
-    void Reject();
-    void Cancel();
-    void Approved();
-    void StartPump();
-    void Pump();
-    void StopPump();
-    void SelectGas(int g);
-    void Receipt();
-    void NoReceipt();
-    void Continue();
-    void ChangeState(int x);
+class State
+{
+public:
+  MDAEFSM *p;
+  State(MDAEFSM *mda);
+  State() : p(nullptr){};
+  virtual void Activate();
+  virtual void Start();
+  virtual void PayType(int t);
+  virtual void Reject();
+  virtual void Cancel();
+  virtual void Approved();
+  virtual void StartPump();
+  virtual void Pump();
+  virtual void StopPump();
+  virtual void SelectGas(int g);
+  virtual void Receipt();
+  virtual void NoReceipt();
+  virtual void Continue();
 };
 
-class State {
-    MDAEFSM *p;
-  public:
-    virtual void Activate();
-    virtual void Start();
-    virtual void PayType(int t);
-    virtual void Reject();
-    virtual void Cancel();
-    virtual void Approved();
-    virtual void StartPump();
-    virtual void Pump();
-    virtual void StopPump();
-    virtual void SelectGas(int g);
-    virtual void Receipt();
-    virtual void NoReceipt();
-    virtual void Continue();
+class Start : public State
+{
+public:
+  Start(MDAEFSM *mda);
+  void Activate() override;
 };
 
-class Start : public State {
-  public:
-    void Activate() override;
+class S0 : public State
+{
+public:
+  S0(MDAEFSM *mda);
+  void Start() override;
 };
 
-class S0 : public State {
-  public:
-    void Start() override;
+class S1 : public State
+{
+public:
+  S1(MDAEFSM *mda);
+  void PayType(int t) override;
 };
 
-class S1 : public State {
-  public:
-    void PayType(int t) override;
+class S2 : public State
+{
+public:
+  S2(MDAEFSM *mda);
+  void Reject() override;
+  void Approved() override;
 };
 
-class S2 : public State {
-  public:
-    void Reject() override;
-    void Approved() override;
+class S3 : public State
+{
+public:
+  S3(MDAEFSM *mda);
+  void Cancel() override;
+  void Continue() override;
+  void SelectGas(int g) override;
 };
 
-class S3 : public State {
-  public:
-    void Cancel() override;
-    void Continue() override;
-    void SelectGas(int g) override;
+class S4 : public State
+{
+public:
+  S4(MDAEFSM *mda);
+  void StartPump() override;
 };
 
-class S4 : public State {
-  public:
-    void StartPump() override;
+class S5 : public State
+{
+public:
+  S5(MDAEFSM *mda);
+  void Pump() override;
+  void StopPump() override;
 };
 
-class S5 : public State {
-  public:
-    void Pump() override;
-    void StopPump() override;
+class S6 : public State
+{
+public:
+  S6(MDAEFSM *mda);
+  void NoReceipt() override;
+  void Receipt() override;
 };
 
-class S6 : public State {
-  public:
-    void NoReceipt() override;
-    void Receipt() override;
+class MDAEFSM
+{
+public:
+  State *s;
+  State *LS[8];
+  Start start;
+  S0 s0;
+  S1 s1;
+  S2 s2;
+  S3 s3;
+  S4 s4;
+  S5 s5;
+  S6 s6;
+  MDAEFSM();
+  void Activate();
+  void Start();
+  void PayType(int t);
+  void Reject();
+  void Cancel();
+  void Approved();
+  void StartPump();
+  void Pump();
+  void StopPump();
+  void SelectGas(int g);
+  void Receipt();
+  void NoReceipt();
+  void Continue();
+  void ChangeState(int x);
 };
 
-class DataStore {
-  public:
+class DataStore
+{
+public:
 };
 
-class DataStore1 : public DataStore {
-  public:
-    int temp_a;
-    int price;
-    int L;
-    int total;
-    int temp_cash;
-    int w;
-    int cash;
+class DataStore1 : public DataStore
+{
+public:
+  int temp_a;
+  int price;
+  int L;
+  int total;
+  int temp_cash;
+  int w;
+  int cash;
 };
 
-class DataStore2 : public DataStore {
-  public:
-    float temp_a;
-    float temp_b;
-    float temp_c;
-    int temp_cash;
-    float Dprice;
-    float Rprice;
-    float Pprice;
-    int cash;
-    int total;
-    int G;
-    float price;
+class DataStore2 : public DataStore
+{
+public:
+  float temp_a;
+  float temp_b;
+  float temp_c;
+  int temp_cash;
+  float Dprice;
+  float Rprice;
+  float Pprice;
+  int cash;
+  int total;
+  int G;
+  float price;
 };
 
-class GP1 {
-    MDAEFSM *m;
-    DataStore *d;
-  public:
-    GP1(DataStore1 *ds);
-    void Activate(int a);
-    void Start();
-    void PayCredit();
-    void Reject();
-    void Cancel();
-    void Approved();
-    void PayCash(int c);
-    void StartPump();
-    void Pump();
-    void StopPump();
+class GP1
+{
+  MDAEFSM *m;
+  DataStore *d;
+
+public:
+  GP1(DataStore1 *ds, MDAEFSM *mda);
+  void Activate(int a);
+  void Start();
+  void PayCredit();
+  void Reject();
+  void Cancel();
+  void Approved();
+  void PayCash(int c);
+  void StartPump();
+  void Pump();
+  void StopPump();
 };
 
-class GP2 {
-    MDAEFSM *m;
-    DataStore *d;
-  public:
-    GP2(DataStore2 *ds);
-    void Activate(float a, float b, float c);
-    void Start();
-    void PayCash(int c);
-    void Cancel();
-    void Premium();
-    void Regular();
-    void Diesel();
-    void StartPump();
-    void PumpGallon();
-    void Stop();
-    void Receipt();
-    void NoReceipt();
+class GP2
+{
+  MDAEFSM *m;
+  DataStore *d;
+
+public:
+  GP2(DataStore2 *ds);
+  void Activate(float a, float b, float c);
+  void Start();
+  void PayCash(int c);
+  void Cancel();
+  void Premium();
+  void Regular();
+  void Diesel();
+  void StartPump();
+  void PumpGallon();
+  void Stop();
+  void Receipt();
+  void NoReceipt();
 };
 
 #endif /* MAIN_H */
